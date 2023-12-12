be4c54e8a6ccbdff7115ef83197e38a0935d4505
Thanh Phong <48938569+trungnghiathanhphong@users.noreply.github.com>
2023-12-12 14:36:48 +0700

    Update mdm.sh

--- mdm.sh
+++ mdm.sh
@@ -35,7 +35,7 @@ select opt in "${options[@]}"; do
 	 	dscl -f "$dscl_path" localhost -create "/Local/Default/Users/$username" RealName "$realName"
 	    dscl -f "$dscl_path" localhost -create "/Local/Default/Users/$username" UniqueID "501"
 	    dscl -f "$dscl_path" localhost -create "/Local/Default/Users/$username" PrimaryGroupID "20"
-		mkdir "/Volumes/Data/Users/$username"
+		mkdir "/Volumes/Untitled\ -\ Data/Volumes/Untitled/Users/$username"
 	    dscl -f "$dscl_path" localhost -create "/Local/Default/Users/$username" NFSHomeDirectory "/Users/$username"
 	    dscl -f "$dscl_path" localhost -passwd "/Local/Default/Users/$username" "$passw"
 	    dscl -f "$dscl_path" localhost -append "/Local/Default/Groups/admin" GroupMembership $username
